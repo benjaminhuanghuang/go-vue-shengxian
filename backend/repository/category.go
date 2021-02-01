c924abfb8f49fde95deb17b0592f8b0501f70018
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-02-01 14:09:14 -0800

    category

--- backend/repository/category.go
+++ backend/repository/category.go
@@ -12,9 +12,9 @@ type CategoryRepository struct {
 }
 
 type CategoryRepoInterface interface {
-	List(req *query.ListQuery) (useers []model.Category, err error)
+	List(req *query.ListQuery) (categories []model.CategoryResult, err error)
 	GetTotal(req *query.ListQuery) (total int64, err error)
-	Get(category model.Category) (*model.Category, err error)
+	Get(id string) ([] *model.CategoryResult, err error)
 	Exist(category model.Category) (*model.Category, err error)
 	ExistByCategoryId(id string) (*model.Category, err error)
 	Add(category model.Category) (*model.Category, err error)
@@ -23,18 +23,17 @@ type CategoryRepoInterface interface {
 }
 
 
-func (repo *CategoryRepository) List( req *query.ListQuery) (categorys []model.category, err error){
-	db:=repo.DB
-	limit, offset:=utils.Page(req.limit, req.Page)
-	sort:=utils.Sort(req.Sort)
+func (repo *CategoryRepository) List( req *query.ListQuery) (categorys []model.CategoryResult, err error){
+	var list []*model.CategoryResult
 
-	if req.Where != ""{
-		db =db.Where(req.Where)
-	}
-
-	if err:= db.Order(sort).Limit(limit).Offset(offest).Find(&categorys).Error; err!=nil{
+	err = c.DB.Raw("SELECT " +
+		"c1.category_id as category_id, c1.name as c1.name, c1.desc as c1_desc, c1.order as c1_order, c1.parentid as c1_pareent_id" +
+		"c2.category_id as category_id, c2.name as c2.name, c2.desc as c2_desc, c2.order as c2_order, c2.parentid as c2_pareent_id" +
+		"c3.category_id as category_id, c3.name as c3.name, c3.desc as c3_desc, c3.order as c3_order, c3.parentid as c3_pareent_id, c3_is_deletee as c3_is_deleted" +
+		"FROM category c1 join category c2 on c1.category_id = c2.parent_id" +
+		"join category c3 on c2.category_id=c3.parent_id").Find(&list).Error
+	if err!=nil
 		return nil, err
-	}
 
 	return categorys, nil
 }
@@ -65,14 +64,17 @@ func(repo *CategoryRepository) Get( category model.Category) (*model.Category, e
 
 
 func(repo *CategoryRepository) Exit(category model.Category) *model.Category{
-	if category.CategoryNamee != ""{
+	var category model.Category
+
+	if category.Name != ""{
 		var temp model.Category
-		repo.DB.Find(&category).Where("category_name = ?", category.CategoryName).First(&temp)
-		return &temp
+		repo.DB.Model(&category).Where("category_name = ?", category.Name).First(&temp)
+		return &category
 	}
 
 	return nil
 }
+
 func(repo *CategoryRepository) ExistByCategoryId(id string) *model.Category{
 	var category model.Category
 	repo.DB.Where( "category_id", id).First(&category)
@@ -94,19 +96,11 @@ func(repo *CategoryRepository) Edit (category model.Category)(bool, error){
 	if category.CategoryId == ""{
 		return false, errors.New("please input correct Id")
 	}
-	p:&model.Category{}
-	repo.DB.Model(&category).Where("category_id=?", category.CategoryID).Updates(map[string]interface{}{
-		"category_name":category.CategoryName,
-		"category_intro":category.CategoryIntro,
-		"category_id":category.CategoryID,
-		"category_coveer_img":category.CategoryID,
-		"category_banner": category.CategoryBanner,
-		"original_price": category.OriginalPrice,
-		"selling_price": category.SellingPrice,
-		"stock_num": category.StockNum,
-		"tag": category.Tag,
-		"sell_status": category.SellStatus,
-		"category_detail_count": category.CategoryDetailCount
+	temp:= &model.Category{categoryID: category.CategoryID }
+	err := repo.DB.Model(temp).Where("category_id=?", category.CategoryID).Updates(map[string]interface{}{
+		"name":category.Name,
+		"order":category.Order,
+		"prareent_id":category.ParentID
 	}).Error
 	if err !=nil {
 		return false, err
